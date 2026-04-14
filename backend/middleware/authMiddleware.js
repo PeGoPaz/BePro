0268b7d5f189c8e2b8e327ea3d683aa7210977ea
Artem Postnov <artempostnov2203@gmail.com>
2026-04-14 12:21:47 +0100

    Build structure. Function isAuthenticated.

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,6 @@
+function isAuthenticated(req, res, next) {
+    if (!req.session || !req.session.userId) {
+        return res.status(401).json({ message: "User not authenticated" });
+    }
+    next();
+}
