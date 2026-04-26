fca149bece87c42af266afed776afaa1b6fd5b9a
PeGoPaz <raynov.vladimir@gmail.com>
2026-04-26 17:28:09 +0100

    choto sdelal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
 
 const App = () => {
   return (
-    <div classname='text-green-500'>
-      Testing Tailwing 
+    <div classname='mx-4 sm:mx-[10%]'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
     </div>
   )
 }
